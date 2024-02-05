b3d06c0e2c54659a343a60d581f57516f1e0dbc1
Sashok <zh.alexander.qq@gmail.com>
2024-02-05 03:53:50 +0300

    fix task 1

--- src/main/java/org/example/StartApp.java
+++ src/main/java/org/example/StartApp.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.data.Student;
+import org.example.data.Teacher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,10 +13,14 @@ public class StartApp {
         var student1 = applicationContext.getBean("student1", Student.class);
         var student2 = applicationContext.getBean("student2", Student.class);
         var student3 = applicationContext.getBean("student3", Student.class);
+        var teacher = applicationContext.getBean(Teacher.class);
+
 
         System.out.println(student1 + "\n" +
                 student2 + "\n" +
                 student3);
+
+        System.out.println(teacher);
 
     }
 }
--- src/main/java/org/example/data/Teacher.java
+++ src/main/java/org/example/data/Teacher.java
@@ -1,11 +1,22 @@
 package org.example.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
+@Component
 public class Teacher {
     private String name;
+
     private List<Student> students;
+
+    @Autowired
+    public Teacher(List<Student> students) {
+        this.name = "Dima";
+        this.students = students;
+    }
+
 
     public String getName() {
         return name;
@@ -21,5 +32,13 @@ public class Teacher {
 
     public void setStudents(List<Student> students) {
         this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", students=" + students +
+                '}';
     }
 }
